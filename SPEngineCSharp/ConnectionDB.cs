09968ed8e5d4d61a6353ca2e963ef4a274d78721
McAdam17 <adanmotacruz17@gmail.com>
2020-07-29 18:44:47 -0500

    Class ConnectionDB added

---
+++ SPEngineCSharp/ConnectionDB.cs
@@ -0,0 +1,27 @@
+﻿namespace SPEngineCSharp
+{
+    public static class ConnectionDB
+    {
+        /// <summary>
+        /// Is the default string connection
+        /// </summary>
+        public static string DefaultSringConnection { get; set; }
+        /// <summary>
+        /// Get the default connection, that was before configured
+        /// </summary>
+        /// <returns>Defalult connection</returns>
+        public static System.Data.SqlClient.SqlConnection Conection()
+        {
+            return new System.Data.SqlClient.SqlConnection(DefaultSringConnection);
+        }
+        /// <summary>
+        /// Get a new connection, that is now configured
+        /// </summary>
+        /// <param name="conectionString">Is a new String connection</param>
+        /// <returns>A new connection based on conectionString</returns>
+        public static System.Data.SqlClient.SqlConnection Conection(string conectionString)
+        {
+            return new System.Data.SqlClient.SqlConnection(conectionString);
+        }
+    }
+}
