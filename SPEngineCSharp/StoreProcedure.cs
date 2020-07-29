0c8abde4f4878695cc91552c4a59abc2af30fcc3
McAdam17 <adanmotacruz17@gmail.com>
2020-07-29 18:46:06 -0500

    Class StoreProcedure added

---
+++ SPEngineCSharp/StoreProcedure.cs
@@ -0,0 +1,8 @@
+﻿namespace SPEngineCSharp
+{
+    public class StoreProcedure
+    {
+        public string Name { get; set; }
+        public System.Collections.Generic.List<Parameter> Parameters { get; set; }
+    }
+}
